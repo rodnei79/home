5fb218087f56fe15abb1302df53bd77d2aba8bf5
rodnei <rodnei79@gmail.com>
2013-10-10 16:33:44 -0300

    teste de envio

---
+++ app/views/contatos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @contato, :nome, :telefone, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Home::Application.config.secret_key_base = 'd9adf55e9f87c55764183c3223582b9a5aaf243ea042b6404ed5b0da7b3f4dce6d97d819190092ce3f9a1a0d77a5cd61c57b25dea6d738d46da6fa67480e7787'
